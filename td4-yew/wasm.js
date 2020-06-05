9a8eec128a7e35f4986ce157157e43b11c9fd782
ymizushi <mizushi@gmail.com>
2020-06-05 17:07:36 +0900

    update content

--- td4-yew/wasm.js
+++ td4-yew/wasm.js
@@ -448,8 +448,8 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper207 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 67, __wbg_adapter_16);
+    imports.wbg.__wbindgen_closure_wrapper202 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 63, __wbg_adapter_16);
         return addHeapObject(ret);
     };
 
